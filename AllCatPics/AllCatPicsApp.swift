12576030d763c3c314ea50641736648d59b20430
jatezzz <johnandrestrujillo@gmail.com>
2024-02-23 08:57:10 -0500

    Initial Commit

---
+++ AllCatPics/AllCatPicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllCatPicsApp.swift
+//  AllCatPics
+//
+//  Created by John Trujillo on 23/2/24.
+//
+
+import SwiftUI
+
+@main
+struct AllCatPicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
