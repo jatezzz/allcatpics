ea1f24a9465077f3cd3233517e8f5410d31be5e6
jatezzz <johnandrestrujillo@gmail.com>
2024-02-23 22:15:36 -0500

    Add error test case

--- AllCatPicsTests/CatRepositoryTest.swift
+++ AllCatPicsTests/CatRepositoryTest.swift
@@ -10,7 +10,7 @@ import XCTest
 
 final class CatRepositoryTest: XCTestCase {
 
-    func testLoadNextPageSuccess() async throws {
+    func testLoadNextPage_WhenSuccess_AppendsNewCats() async throws {
         let mockCatAPI = MockCatAPI()
         let mockCatLocalStorage = MockCatLocalStorage()
         mockCatAPI.catsToReturn = [.make()]
@@ -26,17 +26,26 @@ final class CatRepositoryTest: XCTestCase {
         XCTAssertEqual(newCats!.first?.id, "123")
     }
     
-    func testLoadNextPageFailure() async throws {
+    func testLoadNextPage_WhenFailure_CapturesError() async throws {
+        // Arrange
         let mockCatAPI = MockCatAPI()
         let mockCatLocalStorage = MockCatLocalStorage()
         mockCatAPI.errorToThrow = NSError(domain: "TestError", code: 1, userInfo: nil)
         
         let repository = CatRepository(api: mockCatAPI, localStorage: mockCatLocalStorage)
         
-        // Perform the action
-        let newCats = try? await repository.getList(page: 0)
+        // Act
+        var expectedError: Error?
+        do {
+            _ = try await repository.getList(page: 0)
+        } catch {
+            expectedError = error
+        }
         
-        // Assert the final state
-        XCTAssertNil(newCats)
+        // Assert
+        XCTAssertNotNil(expectedError)
+        XCTAssertEqual((expectedError as NSError?)?.domain, "TestError")
+        XCTAssertEqual((expectedError as NSError?)?.code, 1)
     }
+
 }
