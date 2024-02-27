f5f49879da4db52a1cd324d1ffd6b11c87b2d1fd
jatezzz <johnandrestrujillo@gmail.com>
2024-02-27 00:04:38 -0500

    Geometry reader added.

--- AllCatPics/Presentation/List/Card.swift
+++ AllCatPics/Presentation/List/Card.swift
@@ -4,7 +4,6 @@
 //
 //  Created by John Trujillo on 23/2/24.
 //
-
 import SwiftUI
 import Kingfisher
 
@@ -13,26 +12,30 @@ struct Card: View {
     @Environment(\.theme) var theme: Theme
 
     var body: some View {
-        let screenWidth = UIScreen.main.bounds.width / 2 - 30
-        
-        VStack {
-            ZStack(alignment: .bottomLeading) {
-                KingfisherImageView(url: CatAPIEndpoints.catImageURL(id: cat.id), width: screenWidth, height: screenWidth, cornerRadius: 10, contentMode: .fill)
-                Text(cat.displayName)
-                    .bold()
-                    .foregroundColor(.white)
-                    .padding(5)
-                    .background(Color.black.opacity(0.5))
-                    .cornerRadius(5)
-                    .padding([.leading, .bottom], 8)
-                    .frame(maxWidth: .infinity)
+        GeometryReader { geometry in
+            let screenWidth = geometry.size.width
+            
+            VStack {
+                ZStack(alignment: .bottomLeading) {
+                    KingfisherImageView(url: CatAPIEndpoints.catImageURL(id: cat.id), width: screenWidth, height: screenWidth, cornerRadius: 10, contentMode: .fill)
+                    Text(cat.displayName)
+                        .bold()
+                        .foregroundColor(.white)
+                        .padding(5)
+                        .background(Color.black.opacity(0.5))
+                        .cornerRadius(5)
+                        .padding([.leading, .bottom], 8)
+                        .frame(maxWidth: .infinity)
+                }
+                .frame(maxWidth: .infinity)
+                .themedStyle(Theme.TextStyle(font: .headline, color: .black))
             }
-            .frame(width: screenWidth, height: screenWidth)
-            .themedStyle(Theme.TextStyle(font: .headline, color: .black))
+            .frame(maxWidth: .infinity)
         }
-        .frame(width: screenWidth)
+        .aspectRatio(1, contentMode: .fit)
     }
 }
+
 
 #Preview {
     Card(cat: Cat(tags: ["tag1"], createdAt: nil, updatedAt: nil, mimetype: nil, size: nil, id: "Hola", editedAt: nil))
